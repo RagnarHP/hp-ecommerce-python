1b9a38183ddcee679cccdc058f077bd65ce31071
ragnarHP <ragnar@handpoint.com>
2013-05-30 23:43:02 +0000

    Example Authorization

---
+++ ExampleAuthorizationReversal.py
@@ -0,0 +1,18 @@
+__author__ = 'ragnar'
+
+import api
+
+print "Test"
+
+api.url = "https://tweb34.handpoint.com"
+api.cardAcceptor = "7f6451e8314defbb50d0"
+api.sharedSecret = "8F10C8AD35B7AEC11675B50DBF6ACEAA0B4EC280B92500E51A02F7BBBE7B07C6"
+
+paymentData = api.PaymentData(currency="USD", amount=1.00, card_number="4242424242424242", expiry_date_year="1215",
+                              card_verification_code="123")
+
+response = api.authorize(paymentData)
+
+reversalResponse = api.authorizationReversal(api.getAuthorizationGuid(response.read()))
+
+print('Response from webstore: %s' % reversalResponse.read())
